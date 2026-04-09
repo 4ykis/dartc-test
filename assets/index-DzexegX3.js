dbfdce6efb410dd77b9090c104e83041bf56b8c3
Roman Yasinskiy <4yk.roman@gmail.com>
2026-04-10 00:03:08 +0300

    Updates

--- assets/index-DzexegX3.js
+++ assets/index-DzexegX3.js
@@ -38,16 +38,16 @@
     <div class="navbar">
       <div class="navbar__wrapper">
         <a href="#" class="navbar__link icon-button">
-          <svg width="24" height="24"><use href="/icons.svg#home"></use></svg>
+          <svg width="24" height="24"><use href="./icons.svg#home"></use></svg>
         </a>
         <a href="#" class="navbar__link icon-button">
-          <svg width="24" height="24"><use href="/icons.svg#catalog"></use></svg>
+          <svg width="24" height="24"><use href="./icons.svg#catalog"></use></svg>
         </a>
         <a href="#" class="navbar__link icon-button">
-          <svg width="24" height="24"><use href="/icons.svg#favorite"></use></svg>
+          <svg width="24" height="24"><use href="./icons.svg#favorite"></use></svg>
         </a>
         <a href="#" class="navbar__link icon-button">
-          <svg width="24" height="24"><use href="/icons.svg#shopping-basket"></use></svg>
+          <svg width="24" height="24"><use href="./icons.svg#shopping-basket"></use></svg>
         </a>
       </div>
     </div>
